16136f03a98303359cb744280e993a28d72c70db
Jie Hou <jie.hou@prosiebensat1.com>
2020-12-22 16:04:36 +0100

    change name

--- test_navigation/test_navigation_lop/test_cal_line_of_position.py
+++ test_navigation/test_navigation_lop/test_cal_line_of_position.py
@@ -7,11 +7,11 @@ LoP = namedtuple('LOP', 'location copass_course slop intercept')
 
 
 @pytest.fixture(scope='module')
-def cal_lop(compass_course=225, location=position(name='test', latitude=15, longitude=15)
+def cal_lop(compass_course=225, location=position(name='test_navigation', latitude=15, longitude=15)
             , devication_table_path='navigation/magnetic_deviation_table.csv'):
     return CalculateShipPosition(compass_course, location, devication_table_path)
 
 
 def test_cal_line_vector(cal_lop):
     lop = cal_lop.cal_line_vector()
-    assert (lop == LoP(position('test', 15, 15), 225, 0.6, 6.0))
+    assert (lop == LoP(position('test_navigation', 15, 15), 225, 0.6, 6.0))
