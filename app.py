57f5a170e90aef043e284c8e08dc1909e6a26b01
claire0701 <claire.hou0701@gmail.com>
2021-05-13 11:30:30 +0200

    added postgressql env

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from application.resources.course import Course, CourseList
 from application.db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL_NEW', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'test'
 api = Api(app)
